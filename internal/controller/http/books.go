180d978cdc25cef292d5828e83a8047045362766
Uriel Robles <urielrobles802@gmail.com>
2023-02-13 14:53:26 -0600

    rename variable

--- internal/controller/http/books.go
+++ internal/controller/http/books.go
@@ -5,10 +5,10 @@ import (
 	entity "github.com/roblesoft/bookd/internal/entity"
 )
 
-func (s *HTTP) GetAll(c *fiber.Ctx) error {
+func (h *HTTP) GetAll(c *fiber.Ctx) error {
 	limit := c.Query("limit")
 	page := c.Query("page")
-	items, err := s.Service.GetAllBooks(limit, page)
+	items, err := h.Service.GetAllBooks(limit, page)
 	if err != nil {
 		c.Status(500).JSON(fiber.Map{
 			"error": err.Error(),
@@ -18,9 +18,9 @@ func (s *HTTP) GetAll(c *fiber.Ctx) error {
 	return c.JSON(items)
 }
 
-func (s *HTTP) GetBook(c *fiber.Ctx) error {
+func (h *HTTP) GetBook(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
-	item, err := s.Service.GetBook(id)
+	item, err := h.Service.GetBook(id)
 	if err != nil {
 		c.Status(500).JSON(fiber.Map{
 			"error": err.Error(),
@@ -30,7 +30,7 @@ func (s *HTTP) GetBook(c *fiber.Ctx) error {
 	return c.JSON(item)
 }
 
-func (s *HTTP) CreateBook(c *fiber.Ctx) error {
+func (h *HTTP) CreateBook(c *fiber.Ctx) error {
 	var book entity.Book
 	if err := c.BodyParser(&book); err != nil {
 		c.Status(400).JSON(fiber.Map{
@@ -38,7 +38,7 @@ func (s *HTTP) CreateBook(c *fiber.Ctx) error {
 		})
 		return err
 	}
-	if err := s.Service.CreateBook(&book); err != nil {
+	if err := h.Service.CreateBook(&book); err != nil {
 		c.Status(500).JSON(fiber.Map{
 			"error": err.Error(),
 		})
@@ -47,7 +47,7 @@ func (s *HTTP) CreateBook(c *fiber.Ctx) error {
 	return c.JSON(book)
 }
 
-func (s *HTTP) UpdateBook(c *fiber.Ctx) error {
+func (h *HTTP) UpdateBook(c *fiber.Ctx) error {
 	id, _ := c.ParamsInt("id")
 	var book entity.Book
 	if err := c.BodyParser(&book); err != nil {
@@ -57,7 +57,7 @@ func (s *HTTP) UpdateBook(c *fiber.Ctx) error {
 		return err
 	}
 	book.ID = id
-	if err := s.Service.UpdateBook(&book); err != nil {
+	if err := h.Service.UpdateBook(&book); err != nil {
 		c.Status(500).JSON(fiber.Map{
 			"error": err.Error(),
 		})
@@ -66,9 +66,9 @@ func (s *HTTP) UpdateBook(c *fiber.Ctx) error {
 	return c.JSON(book)
 }
 
-func (s *HTTP) DeleteBook(c *fiber.Ctx) error {
+func (h *HTTP) DeleteBook(c *fiber.Ctx) error {
 	id, _ := c.ParamsInt("id")
-	if err := s.Service.DeleteBook(id); err != nil {
+	if err := h.Service.DeleteBook(id); err != nil {
 		c.Status(500).JSON(fiber.Map{
 			"error": err.Error(),
 		})
